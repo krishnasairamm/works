b9bfc4a743c128f152b68b2036d2f0b2463a06f8
krishnasairamm <124793280+krishnasairamm@users.noreply.github.com>
2024-08-01 15:45:43 +0530

    Delete express1.js

--- express1.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/hooman',function(req,res){
-    res.send("Welcome to exprerss js testing by human..!!!!");
-});
-
-app.listen(3000);
